4866541098c7de89f60ac425b09d4ac84ec42e25
jattapol456 <jattapol.aom@gmail.com>
2022-01-25 11:50:37 +0700

    feat: linkpage

--- components/menus/Sidebar.vue
+++ components/menus/Sidebar.vue
@@ -2,11 +2,16 @@
 .flex.flex-col
     aside.bg-red-main.relative.h-screen.w-52
         nav.text-white.text-base.space-y-1
-            button(class="text-xl bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Request
-            button(class="text-xl bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Treasury
-            button(class="text-xl bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Latest
-            button(class="text-xl bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Frequent
-            button(class="text-xl bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Add device
+            nuxt-link(to="/")
+                button(class="text-xl text-left pl-8 bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Request
+            nuxt-link(to="/treasury")
+                button(class="text-xl text-left pl-8 bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Treasury
+            nuxt-link(to="/latest") 
+                button(class="text-xl text-left pl-8 bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Latest
+            nuxt-link(to="/frequent")
+                button(class="text-xl text-left pl-8 bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Frequent
+            nuxt-link(to="/adddevice")
+                button(class="text-xl text-left pl-8 bg-red-main focus:bg-red-button1 focus:ring focus:ring-red-200 w-full h-16") Add device
 
 </template>
 <script lang="js">
